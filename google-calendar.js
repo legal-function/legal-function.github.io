e243001c585dd63c01a5374b0c323850612ebdcf
Lady Marian <legal.function@gmail.com>
2016-08-01 19:33:29 +0200

    Add urls

--- google-calendar.js
+++ google-calendar.js
@@ -6,32 +6,19 @@ angular.module('GoogleCalendar', []).directive('googleCalendar', function(){
       hash: '=?'
     },
     templateUrl: 'google-calendar.html',
-    controller: ['$filter', '$http', '$scope', function($filter, $http, $scope){
+    controller: function($filter, $http, $scope) {
 
       if (!$scope.gcConfig.google_key) {
         throw('Missing required config google_key');
-      }
+      };
 
       if (!$scope.gcConfig.calendar_id) {
         throw('Missing required config calendar_id');
-      }
-
-      /**
-       * Must check if ngSanitize is
-       * a part of the project for
-       * binding description html
-       */
-      try {
-        angular.module("ngSanitize");
-        sanitize =  true;
-      } catch (err) {
-        sanitize = null;
-      }
+      };
 
       var defaults = {
         max: 3,
         hideTitle: false,
-        sanitize: sanitize,
         dateTimeFilter: 'd. MMM HH.mm',
         dateFilter: 'd. MMM',
         htmlDesc: true,
@@ -40,13 +27,6 @@ angular.module('GoogleCalendar', []).directive('googleCalendar', function(){
 
       $scope.gcConfig = angular.extend(defaults, $scope.gcConfig);
 
-      /**
-       * TODO: Debug filter used by multiple use of the directive
-       * There is some weird issues with filter if the directive
-       * is used multiple times with different config for filter..
-       * for some reason the last passed filters is used for every use
-       * of the directive
-       */
       fulldayFilter = function(date) {
         return $filter('date')(date, $scope.gcConfig.dateFilter)
       };
@@ -55,40 +35,37 @@ angular.module('GoogleCalendar', []).directive('googleCalendar', function(){
         return $filter('date')(date, $scope.gcConfig.dateTimeFilter);
       };
 
-
-      $scope.start = function(event){
+      start_time = function(event){
         if (event.start.date) {
           return fulldayFilter(event.start.date);
         } else if (event.start.dateTime) {
           return timedFilter(event.start.dateTime);
-        }
+        };
       };
 
-      $scope.end = function(event){
-        /**
-         * The end date is off by one in google for entire day events
-         * so we need to substract one day
-         */
+      end_time = function(event){
         if (event.end.date) {
-          var d = new Date(event.end.date);
-          d.setDate(d.getDate() - 1);
+          var d = new Date(event.end.date)
           return fulldayFilter(d);
         } else if (event.end.dateTime) {
           return timedFilter(event.end.dateTime);
-        }
-
+        };
       };
 
       var url = "https://www.googleapis.com/calendar/v3/calendars/" + $scope.gcConfig.calendar_id + "/events?orderBy=startTime&singleEvents=true&timeMin=" + (new Date().toISOString()) + "&maxResults=" + $scope.gcConfig.max + "&key=" + $scope.gcConfig.google_key;
 
       $http.get(url).success(function(data){
         $scope.calendar = data;
+
+        $scope.calendar.html = "https://www.google.com/calendar/embed?src=" + $scope.gcConfig.calendar_id
+        $scope.calendar.ical = "webcal://www.google.com/calendar/ical/" + $scope.gcConfig.calendar_id + "/public/basic.ics"
+
         if (!$scope.gcConfig.hideTitle && !$scope.gcConfig.calendar_name)
           angular.extend($scope.gcConfig, { calendar_name: data.summary })
 
         angular.forEach($scope.calendar.items, function(item) {
-          item.startTime = $scope.start(item);
-          item.endTime =  $scope.end(item);
+          item.startTime = start_time(item);
+          item.endTime =  end_time(item);
           if (item.start.date) {
             item.startDate = new Date(item.start.date);
           } else {
@@ -98,6 +75,6 @@ angular.module('GoogleCalendar', []).directive('googleCalendar', function(){
         $scope.$parent.all_calendars.push($scope.calendar.items);
       });
     }
-    ]
+
   };
 });
